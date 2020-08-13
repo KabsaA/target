9605085f5e3e9df17e419426a4f95ff4e4e4c96c
Kabsa <kabsaabdi23@gmail.com>
2020-08-12 19:40:43 -0600

    Initial Commit

---
+++ targetTests/targetTests.swift
@@ -0,0 +1,34 @@
+//
+//  targetTests.swift
+//  targetTests
+//
+//  Created by Kabsa Abdi on 8/12/20.
+//  Copyright © 2020 Kabsa Abdi. All rights reserved.
+//
+
+import XCTest
+@testable import target
+
+class targetTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
